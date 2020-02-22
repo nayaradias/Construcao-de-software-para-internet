51a4074337cd15db0a2cc9ea8117452dafb20330
nayaradias <nayaradias@users.noreply.github.com>
2020-02-22 20:37:06 -0300

    aula - rotas

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.post('/message', function(req, res, next){
+    var messageVar = req.body.messageBody;
+    res.redirect('/message/' + messageVar);
+});
+
+router.get('/message/:msgParam', function(req, res, next){
+    res.render('node', { message: req.param.msgParam });
+});
+
 module.exports = router;
