e6afd9bae313e58dac72f8ebd5b58f4cf31a2a23
nayaradias <nayaradias@users.noreply.github.com>
2020-04-17 17:44:47 -0300

    commit sem teste

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from "./app.component";
 import { MessageComponent } from './messages/message.component';
-import {MessageListComponent} from './messages/message-list.component';
-import {MessageInputComponent} from './messages/message-input.component';
-@NgModule({
+import { MessageListComponent} from './messages/message-list.component';
+import { MessageInputComponent } from './messages/message-input.component';
+// import { MessagesComponent } from './messages/messages.component
+// import { AuthenticationComponent } from './auth/authentication.component';
+// import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
+// import { HeaderComponent } from './header.component';
+@NgModule({ 
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent,        
+        MessageInputComponent,
+        // AuthenticationComponent       
     ],
     imports: [BrowserModule,FormsModule],
     bootstrap: [AppComponent]
---
+++ assets/app/auth/authentication.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-authentication',
+    template: '<h1>Componente de Autenticação</h1>'
+})
+export class AuthenticationComponent{
+
+}
